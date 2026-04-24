c0f52052feebefb9d07e7042ab1a1175fc6abb46
blumgardt <471752@niuitmo.ru>
2026-04-24 15:30:14 +0300

    fix: white page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter
+      basename="/fizon"
       future={{
         v7_relativeSplatPath: true,
         v7_startTransition: true,
